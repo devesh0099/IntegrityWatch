6760a29febfbe5683e35690e32c7582eba55366c
devesh0099 <deveshbhushan8@gmail.com>
2025-11-15 22:16:56 +0530

    feat(platform/base.py): Used cpuid instead of py-cpuinfo due to security concerns will include wheel files for windows.

--- src/vm_detector/platform/base.py
+++ src/vm_detector/platform/base.py
@@ -23,10 +23,8 @@ def get_cpuid_registers(leaf: int) -> tuple:
 def get_cpu_vendor() -> str:
     # Get CPUID vendor string from EBX+ECX+EDX at specified leaf.
     try:
-        from cpuinfo import get_cpu_info
-        
-        info = get_cpu_info()
-
+        from cpuid import cpuid
+        import struct
 
         eax, ebx, ecx, edx = cpuid(0)
 
@@ -58,60 +56,23 @@ def get_hypervisor_vendor(leaf: int = 0x40000000) -> str:
         return ""
 
 def get_cpuid_vendor(leaf: int) -> str:
-    try:
-        from cpuinfo import get_cpu_info
-        
-        info = get_cpu_info()
-
-        if leaf == 0:
-            # Get CPU vendor
-            vendor = info.get('vendor_id_raw', '')
-            return vendor
-        
-        elif leaf == 0x40000000 or leaf == 0x40000100:
-            flags = info.get('flags', [])
-
-            if 'hypervisor' in flags:
-                brand = info.get('brand_raw', '').lower()
-
-                # Check for VM keywords in brand/model
-                if 'vmware' in brand:
-                    return 'VMwareVMware'
-                elif 'virtualbox' in brand or 'vbox' in brand:
-                    return 'VBoxVBoxVBox'
-                elif 'qemu' in brand or 'kvm' in brand:
-                    return 'KVMKVMKVM'
-                elif 'microsoft' in brand or 'hyper-v' in brand:
-                    return 'Microsoft Hv'
-                elif 'xen' in brand:
-                    return 'XenVMMXenVMM'
-                else:
-                    # Generic hypervisor detected, but can't identify vendor
-                    return 'hypervisor'
-            return ""
-        
-        return ""
-    except Exception:
-        return ""
+    if leaf == 0:
+        return get_cpu_vendor()
+    else:
+        return get_hypervisor_vendor(leaf)
 
 
 def get_cpuid_features() -> dict:
     # Get CPU feature flags from CPUID leaf 1
     try:
-        from cpuinfo import get_cpu_info
-        
-        info = get_cpu_info()
-        flags = info.get('flags', [])
+        from cpuid import cpuid
+        eax,ebx,ecx,edx = cpuid(1)
 
-        ecx = 0
-        if 'hypervisor' in flags:
-            ecx = (1 << 31)  # Set bit 31
-        
         return {
-            'eax': 0,
-            'ebx': 0,
-            'ecx': ecx,
-            'edx': 0
+            'eax':eax,
+            'ebx':ebx,
+            'ecx':ecx,
+            'edx':edx
         }
-    except Exception:
+    except:
         return {}
